ceea6115ca23138d8abf7ccfae6d2870ccb83004
kepes.peter <kepes.peter@codeplay.hu>
2013-08-05 20:30:26 +0200

    use attributes

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default[:chef_sensu_community_plugins][:path] = "/opt/sensu-community-plugins"
+default[:chef_sensu_community_plugins][:repository] = "https://github.com/sensu/sensu-community-plugins.git"
+default[:chef_sensu_community_plugins][:reference] = "4afa283006ea3ad11dace23cc3dcd826be671d80"
